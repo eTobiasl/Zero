02c3e6d2f93fcc626565fa11750df9af408fae2c
eTobiasl <eTobiasl@users.noreply.github.com>
2022-05-24 00:41:00 +0200

    Added intro screen

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import robot from './robot.png'
 import hi from './Hi.png'
 import bgTrees from './backgroundTrees.png'
 import { InfoScreen } from './InfoScreen';
+import building from './building.png'
 
 import shockEmoji from './shockEmoji.png'
 import waveEmoji from './waveEmoji.png'
@@ -17,6 +18,7 @@ import eWaste from './eWaste.png'
 import phoneBroken from './phoneBroken.png'
 
 import { Button } from '@mui/material';
+
 
 const birdRadius = 22;
 const start = 0
@@ -39,7 +41,6 @@ class App extends Component {
     return pipes;
   }
 
-
   constructor(props) {
     super(props);
     this.state = {
@@ -53,14 +54,19 @@ class App extends Component {
       score: 0,
       hearts: 3,
       textIndex: 0,
-      toggleInfo: true,
+      textIndex2: 0,
+      toggleInfo: false,
       numberOfPhones: 1,
       toggleEndScreen: false,
       toggleHitIndicator: false,
       toggleGarbage: false,
       toggleHitCooldown: false,
+      garbageHeight: "20em",
+      garbageWidth: "100%",
+      toggleIntroScreen: true,
     }
     this.moveUp = this.moveUp.bind(this)
+
   }
 
   componentDidMount() {
@@ -68,7 +74,8 @@ class App extends Component {
   }
 
   update() {
-    console.log(this.state.velocity)
+
+
     if (this.state.hearts <= 0){
       this.setState({toggleGame: false})
     }
@@ -162,7 +169,6 @@ class App extends Component {
     const left = this.state.left;
     const birdHeight = this.state.birdHeight;
     
-
     if (this.state.toggleGame){
         return (
           <div className="App" style = {{pointerEvents: "none"}}>
@@ -175,9 +181,9 @@ class App extends Component {
               <img src = {bird} style = {{width: "3em", transform: "scale(2)"}}></img>
               {/* <Circle r={birdRadius} fill={{ color: '#2409ba' }} stroke={{ color: '#E65243' }} strokeWidth={3} /> */}
             </div>
-            <h1 style = {{color: "black", position: "absolute", top: "1em", right: "1em", zIndex: "2", fontSize: "2.5em"}}>{Math.round(this.state.score/1000)} år / {this.state.numberOfPhones} telefon(er)</h1>
+            <h1 style = {{color: "black", position: "absolute", top: "1em", right: "1em", zIndex: "2", fontSize: "2em"}}>{Math.round(this.state.score/1000)} år / {this.state.numberOfPhones} telefon(er)</h1>
             <h1 style = {{color: "black", position: "absolute", top: "1em", left: "1em", zIndex: "2", fontSize: "2em"}}>Hjerter: {this.state.hearts}</h1>
-            { this.state.toggleGarbage && <img src = {eWaste} style = {{position: "absolute", bottom: "-5em", width: "100%", height: "20em", zIndex: "4"}}></img>}
+            { this.state.toggleGarbage && <img src = {eWaste} style = {{position: "absolute", bottom: "-5em", width: this.state.garbageWidth, height: this.state.garbageHeight, zIndex: "4"}}></img>}
             {this.state.pipes.map(pipe => {
               let upperPipeHeight = pipe.upperPipeHeight;
               const x = pipe.x;
@@ -196,12 +202,32 @@ class App extends Component {
           </div>
         );
       }
+    if(this.state.toggleIntroScreen){
+      return (
+        <div className = "App" style = {{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
+          <h1 style = {{fontSize: "10em"}}>Zero</h1>
+          <h2 style = {{color: "grey", marginTop: "-5em"}}>Lær mer om e-avfall gjennom spill</h2>
+          <Button variant = "contained" onClick = {()=>{this.setState({toggleIntroScreen: false, toggleInfo: true})}} style = {{width: "15em", height: "2em", fontSize: "1.5em"}}>Start</Button>
+
+          <img src = {building} style = {{position: "absolute", top: "0em", left: "4em", width: "15%"}}></img>
+          <img src = {building} style = {{position: "absolute", bottom: "-10em", left: "4em", width: "15%"}}></img>
+
+          <img src = {building} style = {{position: "absolute", top: "-20em", right: "8em", width: "20%"}}></img>
+          <img src = {building} style = {{position: "absolute", bottom: "-8em", right: "8em", width: "20%"}}></img>
+
+          <img src = {bird} style = {{position: "absolute", top: "15em", right: "8em", width: "10%"}}></img>
+
+          <img src={eWaste} style = {{width: "25%", position: "absolute", bottom: "0em", right: "10em"}}></img>
+        </div>
+      );
+    }
+
     if(this.state.toggleInfo){
       const textList = [
         "Velkommen! I dette spillet skal vi prøve å lære deg litt om e-avfall, gjennom et vellkjent spill-format",
         "Kanskje vi ikke kan unngå alt e-avfall, men vi kan definitivt redusere det!",
         "La oss se hvordan du kan bidra!",
-        "The U.S. EPA estimates that 350,000 mobile phones were dumped every day in 2010. That adds up to over 152 million phones a year.",
+        "U.S. EPA estimerer at 350,000 mobiltelefoner ble kastet hver dag i 2010. Dette utgjør 152 millioner telefoner i løpet av et år.",
         "Det er mye e-avfall!",
         "Telefoner har mange deler i seg, og å kjøpe nye telefoner hele tiden er ikke bærekraftig. La oss se hvor lenge du kan beholde telefonen din!"]
         const emojiList = [
@@ -214,15 +240,6 @@ class App extends Component {
         ]
       return(
         <div className = "App" style = {{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
-             <Button variant = "contained"
-          onClick = {()=>{ if (this.state.textIndex <= 4){
-            this.setState({textIndex: this.state.textIndex + 1})
-            } else{
-              this.setState({toggleInfo: false});
-              this.state.birdHeight = (window.innerHeight / 2) - 400;
-              // this.interval = setInterval(() => this.update(), 15);
-            }}}>
-          Neste</Button>
 
           <Button style = {{position: "absolute", top: "2em", right: "2em"}}
           onClick = {()=>{ this.setState({toggleInfo: false});
@@ -231,7 +248,16 @@ class App extends Component {
             Hopp over Info
           </Button>
          <InfoScreen text= {textList[this.state.textIndex]} emoji={emojiList[this.state.textIndex]}/>
-          <img src = {robot} style = {{width: "20em", marginBottom: "-20em"}}></img>
+         <Button variant = "contained" style = {{marginTop: "-2em", zIndex: "4"}}
+          onClick = {()=>{ if (this.state.textIndex < textList.length-1){
+            this.setState({textIndex: this.state.textIndex + 1})
+            } else{
+              this.setState({toggleInfo: false});
+              this.state.birdHeight = (window.innerHeight / 2) - 400;
+              // this.interval = setInterval(() => this.update(), 15);
+            }}}>
+          Neste</Button>
+          <img src = {robot} style = {{width: "20em", marginTop: "2em", position: "absolute", bottom: "0em"}}></img>
       
 
           
@@ -255,7 +281,12 @@ class App extends Component {
     if (this.state.hearts > 0){
       return(
         <div className = "App" style = {{ pointerEvents: "none", width: "100%", height: "100%", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column"}}>
-          <KeyHandler keyEventName={KEYPRESS} keyValue=" " onKeyHandle={()=>{this.setState({toggleGame: true})}} />
+          <KeyHandler keyEventName={KEYPRESS} keyValue=" " onKeyHandle={()=>{this.setState({toggleGame: true}),
+          this.setState({toggleHitCooldown: true})
+          const timeId = setTimeout(() => {
+            // After 3 seconds set the show value to false
+            this.setState({toggleHitCooldown: false})
+          }, 400)}} />
           <KeyHandler keyEventName={KEYPRESS} keyValue="p" onKeyHandle={()=>{this.setState({toggleGame: true})}} />
           <h1 style = {{userSelect: "none", fontSize: "3em", marginBottom: "-3%"}}>Zero</h1>
           <div className = "menu" style = {{
@@ -272,7 +303,7 @@ class App extends Component {
                 <img src = {hi} style = {{width: "10em"}}></img>
                 <img  style = {{ filter: "brightness(1.1)",width: "10em"}}src = {robot}></img>
               </div>
-              <h3 style = {{marginBottom: "-1.8em",textAlign: "left", marginLeft: "4.5em"}}>Game Controls</h3>
+              <h3 style = {{marginBottom: "-1.8em",textAlign: "left", marginLeft: "4.5em"}}>Spill kontroller</h3>
               <div className = "button">
                 <p style = {{
                   border: "solid black .1em",
@@ -295,25 +326,45 @@ class App extends Component {
       if(this.state.textIndex != 0){
         this.setState({textIndex: 0})
       }
-      const emojiList = []
+      const emojiList = [
+        null,
+        interestingEmoji,
+        null,
+        null,
+        shockEmoji,
+        starEyesEmoji,
+      starEyesEmoji]
       let aboveAvarage = null
       if(this.state.numberOfPhones/Math.round(this.state.score/1000) <= this.state.numberOfPhones/2){
         aboveAvarage = false
       } else{
         aboveAvarage = true
       }
+   
       let phoneArray = new Array(Math.round(this.state.numberOfPhones)).fill(<img src = {phoneBroken} style = {{width: "4em"}}></img>);
       const textList = ["Du brukte " + this.state.numberOfPhones + " telefon(er) i løpet av " + Math.round(this.state.score/1000) + " år, gjennomsnittet ligger på 1 telefon per 2 år",
-    " Dette tilsvarer ca  " + 70*this.state.numberOfPhones + "kg Co2 utslipp"]
+    " Dette tilsvarer ca  " + 70*this.state.numberOfPhones + "kg Co2 utslipp", "Den beste måten å miske e-avfall knyttet til telefoner, er ved å beholde telefonen din så lenge som mulig og reparere den ved behov",
+    "Når dette ikke lenger er mulig kan telefonen da sendes til resirkulering, hvor deler av telefonen kan brukes til nye ting",
+     "Dessverre løser ikke dette hele e-avfall problemet, og med dagens løsning er det hovedsakelig metaller som resirkuleres. Materialer som plastikk blir i liten grad resirkulert fra telefoner",
+    "Derfor er det viktig at vi starter en samtale rundt e-avfall, både så vi som individer blir flinkere, men også så selskaper lager mer bærekraftige telefoner!",
+    "Snakk derfor gjerne med de rundt deg om bærekraft og e-avfall, sammen kan vi skape en mer bærekratig verden!"]
         return(
           <div className = "App" style = {{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
-             <div style = {{marginTop: "10em", width: "35em", maxHeight: "30em"}}>
+             { this.state.textIndex2 < 1 && <div style = {{ width: "35em", maxHeight: "30em", marginTop: "-5em"}}>
               {phoneArray}
-            </div>
-           <InfoScreen text= {textList[this.state.textIndex]} emoji={emojiList[this.state.textIndex]}/>
-            <img src = {robot} style = {{ width: "20em", marginTop: "3em"}}></img>
+            </div>}
+           <InfoScreen text= {textList[this.state.textIndex2]} emoji={emojiList[this.state.textIndex2]}/>
+
+          {this.state.textIndex2 < textList.length -1 && <Button variant = "contained" style = {{marginTop: "-2em", zIndex: "4"}}
+          onClick = {()=>{ if (this.state.textIndex2 < textList.length -1){
+            this.setState({textIndex2: this.state.textIndex2 + 1});
+            console.log(this.state.textIndex2);
+          }}}>
+          Neste</Button>}
+
+            <img src = {robot} style = {{ width: "20em", marginTop: "3em", position: "absolute", bottom: "0em"}}></img>
             <Button style = {{position: "absolute", top: "2em", right: "2em"}} onClick= {()=>{
-              this.setState({toggleGame: false, toggleEndScreen: false, toggleInfo: true, hearts: 3, score: 0, numberOfPhones: 1});
+              this.setState({toggleGame: false, toggleEndScreen: false, toggleInfo: true, hearts: 3, score: 0, numberOfPhones: 1, textIndex2: 0, toggleGarbage: false});
             }}>Start på nytt</Button>
           </div>
         );
@@ -345,6 +396,11 @@ class App extends Component {
                     
                     <Button variant = "contained" style = {{backgroundColor: "lightblue", color: "black", textAlign: "center"}}
                       onClick = {()=>{
+                        if(this.state.garbageHeight == "20em" && this.state.toggleGarbage){
+                          this.setState({garbageHeight: "50em"})
+                        } else if (this.state.garbageHeight == "50em" && this.state.toggleGarbage){
+                          this.setState({garbageHeight: "80em"})
+                        }
                         this.setState({hearts: 3, toggleGame: true, toggleGarbage: true, numberOfPhones: this.state.numberOfPhones + 1, toggleHitCooldown: true})
                         this.state.birdHeight = (window.innerHeight / 2) - 400
                         const timeId = setTimeout(() => {
